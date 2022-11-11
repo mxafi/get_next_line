e91beff2bbc6bdbd089055429c733e7be3a09664
Markus Laaksonen <malaakso@student.hive.fi>
2022-11-11 20:24:52 +0200

    fix header, wip get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,42 @@
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 17:08:34 by malaakso          #+#    #+#             */
-/*   Updated: 2022/11/11 17:13:07 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/11/11 20:24:15 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
-	asd
+	char				*buffer;
+	static ssize_t		n_bytes_read;
+	char				*line;
+	static char			*stash;
+	static size_t		stash_size;
+
+	buffer = malloc(BUFFER_SIZE);
+	if (!buffer)
+		return (NULL);
+	if (!stash)
+		stash = malloc(BUFFER_SIZE);
+	else
+		stash = 
+	while (1)
+	{
+		n_bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (n_bytes_read == 0)
+		{
+			handle_zero_bytes_read();
+			empty_stash();
+			free(buffer);
+			return (NULL);
+		}
+		else if (n_bytes_read == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		
+	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 17:08:53 by malaakso          #+#    #+#             */
-/*   Updated: 2022/11/11 17:55:50 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/11/11 19:52:15 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define GET_NEXT_LINE_H
-# ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
